c7634ef7f34581c57f68dd43d272a0272633e14e
Dima <mister.mitya99@gmail.com>
2020-01-23 22:03:33 +0300

    [lession-1] updated logic for first task

--- lesson 1/first.js
+++ lesson 1/first.js
@@ -1,53 +1,51 @@
+const source = [{ name: 'Pavel', age: 33 }, {name: 'Anton', age: 24}, {name: 'Igor', age: 33}];
+const sourceError = [18, null];
+
 // 1
-// const source = [
-//     '123123',
-//     'sadsad',
-//     'asd',
-//     2,
-//     '',
-//     'ad',
-// ];
-// const param = 'param';
+function getSlicedValuesOne(source, param = 'age') {
+    const result = [];
+    source.forEach(element => {
+        if (element && element.hasOwnProperty(param) && element[param]) {
+            result.push(element[param])
+        }
+    });
+    return result
+}
 
-// function getSlicedValues(source, param) {
-//     return source.map(element => typeof(element) === 'string' && element ? element : param);
-// }
+function getSlicedValuesTwo(source, param = 'age') {
+    return source
+        .filter(element => element && element.hasOwnProperty(param) && element[param])
+        .map(element => element[param])
+}
 
-// console.log(getSlicedValues(source, param));
+console.log(getSlicedValuesOne(source, 'age'));
+console.log(getSlicedValuesTwo(source, 'lastName'));
+console.log(getSlicedValuesTwo(sourceError, 'age'));
 
 // 2
-// const source = [
-//     '123123',
-//     'sadsad',
-//     'asd',
-//     2,
-//     '',
-//     'ad',
-// ];
-// const param = 'param';
+function getSlicedValuesThree(source, param) {
+    return source
+        .filter(element => element && element.hasOwnProperty(param) && element[param])
+        .map(element => element[param])
+        .filter((element, index, array) => index === array.indexOf(element))
+}
 
-// function getSlicedValues(source, param) {
-//     return source.map((element, index) => typeof(element) === 'string' && element ? element : `${param}-${index}`)
-// }
-
-// console.log(getSlicedValues(source, param));
+console.log(getSlicedValuesThree(source, 'age'));
+console.log(getSlicedValuesThree(source, 'lastName'));
+console.log(getSlicedValuesThree(sourceError, 'age'));
 
 // 3
-const source = [
-    '123123',
-    'sadsad',
-    'asd',
-    2,
-    '',
-    'ad',
-];
-
-function getSlicedValues(source, func) {
-    return source.map((element, index) => func(element) ? element : `some text - ${index}`)
+function getSlicedValuesFour(source, filter) {
+    return source
+        .map(element => filter(element, 'age'))
+        .filter(element => element)
+        .filter((element, index, array) => index === array.indexOf(element))
 }
 
-function checkParam(param) {
-    return typeof(param) === 'string' && param
+function checkParam(obj, param) {
+    return obj && obj.hasOwnProperty(param) && obj[param]
 }
 
-console.log(getSlicedValues(source, checkParam));
+console.log(getSlicedValuesFour(source, checkParam));
+console.log(getSlicedValuesFour(source, checkParam));
+console.log(getSlicedValuesFour(sourceError, checkParam));
