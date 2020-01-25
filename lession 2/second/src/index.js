3bb30e85ca17596c4b23eadb185f3704d299082d
Dima <mister.mitya99@gmail.com>
2020-01-25 22:09:56 +0300

    [lession-2] added second task

--- lession 2/second/src/index.js
+++ lession 2/second/src/index.js
@@ -1,5 +1,42 @@
-import _ from 'lodash'
+import _ from 'lodash';
 
-_.get();
+const company = {
+    headDepartment: {
+        director: 'Vasya',
+        salary: 100500
+    },
+    devDepartment: {
+        branches: {
+            'front-end': {
+                director: 'Dima',
+                salary: 1000
+            },
+            java: {
+                director: 'Kolya',
+                salary: 500
+            }
+        },
+        efficiency: 200
+    },
+    salesDepartment: {
+        director: {
+            name: 'Nastya',
+            salary: 1000
+        },
+        efficiency: 200
+    }
+};
 
-_.set();
+const newCompany = _.omit(
+    _.cloneDeep(company),
+    [
+        'headDepartment.salary',
+        'salesDepartment.director.salary',
+        'salesDepartment.efficiency',
+        'devDepartment.branches.front-end.salary',
+        'devDepartment.branches.java.salary',
+        'devDepartment.efficiency'
+    ]
+);
+
+console.log(newCompany)
