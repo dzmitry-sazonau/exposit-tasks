91a683fa7755d149d36a20a0660145bfd2adef37
Dima <mister.mitya99@gmail.com>
2020-02-03 00:54:22 +0300

    [lesson-2] added folder with data for second task

---
+++ lesson 2/second/src/data-source/data.js
@@ -0,0 +1,36 @@
+export const company = {
+    headDepartment: {
+        director: 'Vasya',
+        salary: 100500,
+        newDirector: {
+            director: 'new'
+        }
+    },
+    devDepartment: {
+        branches: {
+            'front-end': {
+                director: 'Dima',
+                salary: 1000
+            },
+            java: {
+                director: 'Kolya',
+                salary: 500
+            },
+            qweqwewe: {
+                director: 'Kolya',
+                salary: 500
+            },
+            director2: {
+                salary: 500
+            }
+        },
+        efficiency: 200
+    },
+    salesDepartment: {
+        director: {
+            name: 'Nastya',
+            salary: 1000
+        },
+        efficiency: 200
+    }
+};
--- lesson 2/second/src/index.js
+++ lesson 2/second/src/index.js
@@ -1,41 +1,6 @@
 import _ from 'lodash';
+import { company } from './data-source/data';
 
-const company = {
-    headDepartment: {
-        director: 'Vasya',
-        salary: 100500,
-        newDirector: {
-            director: 'new'
-        }
-    },
-    devDepartment: {
-        branches: {
-            'front-end': {
-                director: 'Dima',
-                salary: 1000
-            },
-            java: {
-                director: 'Kolya',
-                salary: 500
-            },
-            qweqwewe: {
-                director: 'Kolya',
-                salary: 500
-            },
-            director2: {
-                salary: 500
-            }
-        },
-        efficiency: 200
-    },
-    salesDepartment: {
-        director: {
-            name: 'Nastya',
-            salary: 1000
-        },
-        efficiency: 200
-    }
-};
 const newCompany = {};
 const paths = [];
 
