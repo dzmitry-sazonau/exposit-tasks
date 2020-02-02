79d2a21ea0fc0fb33c81c4b4ba016e0c5b2ffbcb
Dima <mister.mitya99@gmail.com>
2020-02-03 00:44:46 +0300

    [lesson-2] added test for first task

--- lesson 2/first/src/index.js
+++ lesson 2/first/src/index.js
@@ -4,10 +4,18 @@ getCapitalByCountry({countryId: 5, countryName: ''})
     .then(capitalName => console.log(capitalName))
     .catch(error => console.log(error));
 
-getCapitalByCountry({countryId: 0, countryName: 'c'})
+getCapitalByCountry({countryId: 5, countryName: 'C'})
     .then(capitalName => console.log(capitalName))
     .catch(error => console.log(error));
 
 getCapitalByCountry({countryId: {}, countryName: ''})
     .then(capitalName => console.log(capitalName))
+    .catch(error => console.log(error));
+
+getCapitalByCountry({countryId: 1, countryName: ''})
+    .then(country => console.log(country))
+    .catch(error => console.log(error));
+
+getCapitalByCountry({countryId: 6, countryName: 'UUUU'})
+    .then(country => console.log(country))
     .catch(error => console.log(error));
